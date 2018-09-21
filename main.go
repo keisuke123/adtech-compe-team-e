0e8b0b818567e2f26131ac010b6d1955ac3bd4c0
Kamiya Keisuke <luckey.star123@gmail.com>
2018-09-21 16:57:06 +0900

    bidのリクエストをうけるエンドポイントbid_reqの追加

--- main.go
+++ main.go
@@ -4,33 +4,54 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"fmt"
 
 	"github.com/gorilla/mux"
 )
 
 type BID struct {
-	id           string  `json:"id,omitempty"`
-	bidPrice     float64 `json:"bidPrice,omitempty"`
-	advertiserId string  `json:"advertiserId,omitempty"`
-	nurl         string  `json:"nurl,omitempty"`
+	Id           string  `json:"Id,omitempty"`
+	BidPrice     float64 `json:"BidPrice,omitempty"`
+	AdvertiserId string  `json:"AdvertiserId,omitempty"`
+	Nurl         string  `json:"Nurl,omitempty"`
+}
+
+type BID_PARAM struct {
+	Id             string
+	FloorPrice     int
+	DeviceId       string
+	MediaId        string
+	Timestamp      int64
+	OsType         string 
+	BannerSize     int
+	BannerPosition int
+	DeviceType     int
 }
 
 var bid BID
 
 func main() {
 	bid = BID{
-		id:           "asdf",
-		bidPrice:     3.14,
-		advertiserId: "zxcv",
-		nurl:         "aiueo",
+		Id:           "asdf",
+		BidPrice:     3.14,
+		AdvertiserId: "zxcv",
+		Nurl:         "aiueo",
 	}
 	router := mux.NewRouter()
-	router.HandleFunc("/people/{id}", BidHandler).Methods("POST")
+	router.HandleFunc("/bid_req", BidHandler).Methods("POST")
+	router.HandleFunc("/win_notice", BidHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
 
 func BidHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body);
+	var params BID_PARAM
+	err := decoder.Decode(&params)
+	if err != nil {
+		w.Write([]byte("json decode error" + err.Error() + "\n"))
+	}
+	fmt.Println(params.MediaId);
 	// peopleをjsonにエンコードしてwに書き込む？
 	json.NewEncoder(w).Encode(bid)
 }
