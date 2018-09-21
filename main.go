68bafc7fb4159bf42dbbcade066189025e7b83e1
nomikura <b1631775@planet.kanazawa-it.ac.jp>
2018-09-21 09:28:28 +0100

    add win_notice

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
 
 	"github.com/gorilla/mux"
 )
@@ -22,10 +22,16 @@ type BID_PARAM struct {
 	DeviceId       string
 	MediaId        string
 	Timestamp      int64
-	OsType         string 
+	OsType         string
 	BannerSize     int
 	BannerPosition int
 	DeviceType     int
+}
+
+type WIN_NOTICE struct {
+	Id      string
+	Price   float64
+	IsClick int
 }
 
 var bid BID
@@ -39,18 +45,27 @@ func main() {
 	}
 	router := mux.NewRouter()
 	router.HandleFunc("/bid_req", BidHandler).Methods("POST")
-	router.HandleFunc("/win_notice", BidHandler).Methods("POST")
+	router.HandleFunc("/win_notice", WinHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", router))
-
 }
 
 func BidHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body);
+	decoder := json.NewDecoder(r.Body)
 	var params BID_PARAM
 	err := decoder.Decode(&params)
 	if err != nil {
 		w.Write([]byte("json decode error" + err.Error() + "\n"))
 	}
-	// peopleをjsonにエンコードしてwに書き込む？
 	json.NewEncoder(w).Encode(bid)
+}
+
+func WinHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var params WIN_NOTICE
+	err := decoder.Decode(&params)
+	fmt.Println(params)
+	if err != nil {
+		w.Write([]byte("json decode error" + err.Error() + "\n"))
+	}
+	json.NewEncoder(w).Encode(params)
 }
