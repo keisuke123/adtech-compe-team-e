b75643a679b945c8baf560e74c839c0612c4f925
nikkokun <mizutaninikkou@gmail.com>
2018-09-22 13:13:00 +0900

    cleaned files

---
+++ server/tmp.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func main() {
+	// データ追加
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	// people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+	// これなに？
+	router := mux.NewRouter()
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	//サーバーを立ち上げる
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
